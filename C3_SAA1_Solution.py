a4b368bc5e698b2381a45ffcec5a88ad7be97ef1
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-18 13:51:07 +0530

    Add files via upload

---
+++ C3_SAA1_Solution.py
@@ -0,0 +1,18 @@
+# Define a variable "num1" and assign a value to it
+num1 = 90
+# Define a variable "num2" and assign a value to it
+num2 = 50
+# Define a variable "choice" and assign any one value between 1 to 4
+choice = 3
+# Check if "choice" is equal to 1, then print the sum of "num1" and "num2"
+if choice == 1:
+  print(num1 + num2)
+# Check if "choice" is equal to 2, then print the difference between "num1" and "num2"
+if choice == 2:
+  print(num1 - num2)
+# Check if "choice" is equal to 3, then print the product of "num1" and "num2"
+if choice == 3:
+  print(num1 * num2)
+# Check if "choice" is equal to 4, then print the result of "num1" divided by "num2"
+if choice == 4:
+  print(num1 / num2)
